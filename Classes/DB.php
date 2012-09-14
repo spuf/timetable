44efd65ef261bf925363922985f6f9163bd25a24
Arseniy <spufru@gmail.com>
2012-09-14 18:14:57 +0600

    debug

--- Classes/DB.php
+++ Classes/DB.php
@@ -12,20 +12,23 @@ class DB {
 			DB::$dbh = new PDO($dsn, $username, $password, $options);
 			DB::$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (PDOException $e) {
-			die('<b>PDOException: </b>' . $e->getMessage());
+			die('<b>PDOException on connect: </b>' . $e->getMessage());
 		}
 	}
 
-	static function Query($sql, $params = array(), $fetch = true) {
+	static function Query($sql, $params = array(), $fetch = true, &$id = null) {
 		try {
 			$sth = DB::$dbh->prepare($sql);
 			$sth->execute($params);
-			if ($fetch)
+			//Debug::Log($sth->queryString);
+			if ($fetch) {
 				return $sth->fetchAll(PDO::FETCH_ASSOC);
-			else
+			} else {
+				$id = DB::$dbh->lastInsertId();
 				return $sth->rowCount();
+			}
 		} catch (PDOException $e) {
-			die('<b>PDOException: </b>' . $e->getMessage());
+			die('<b>PDOException on query: </b>' . $e->getMessage());
 		}
 	}
 }
---
+++ Classes/SQLSaver.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: spuf
+ * Date: 14.09.12
+ * Time: 11:40
+ * To change this template use File | Settings | File Templates.
+ */
