e9563d905ce95d504d11da6a93c63422e35f0e30
Arseniy <spufru@gmail.com>
2012-09-14 18:15:41 +0600

    storage

---
+++ Classes/Storage.php
@@ -0,0 +1,21 @@
+<?php
+
+class Storage {
+
+	static function Set($key, $value) {
+		DB::Query('INSERT INTO Variables (`Key`, `Value`) VALUES (:key, :value) ON DUPLICATE KEY UPDATE `Value` = :value', array(
+			':key' => $key,
+			':value' => serialize($value),
+		), false);
+	}
+
+	static function Get($key) {
+		$data = DB::Query('SELECT `Value` FROM Variables WHERE `Key` = :key', array(
+			':key' => $key,
+		));
+		if (count($data) > 0) {
+			return unserialize($data[0]['Value']);
+		}
+		return null;
+	}
+}
