ed669f9d70874dd7c4d7e771f57d1c2794309e45
Arseniy <spufru@gmail.com>
2012-09-15 19:47:02 +0600

    olap

--- admin.php
+++ admin.php
@@ -2,8 +2,16 @@
 
 include_once 'bootstrap.php';
 
+$fileId = isset($_GET['file']) ? $_GET['file'] : -1;
 $groupId = isset($_GET['group']) ? $_GET['group'] : -1;
 $dateId = isset($_GET['date']) ? $_GET['date'] : -1;
+
+$files = '';
+$data = DB::Query('SELECT ID, Title, Date FROM Files WHERE Parsed = 1 ORDER BY Title');
+foreach ($data as $item) {
+	$selected = $fileId == $item['ID'] ? 'selected="selected"' : '';
+	$files .= "<option value='{$item['ID']}' $selected>{$item['Title']} - {$item['Date']}</option>";
+}
 
 $groups = '';
 $data = DB::Query('SELECT ID, Title FROM Groups ORDER BY Title');
@@ -21,6 +29,7 @@ foreach ($data as $item) {
 
 print <<<HTML
 <form action="?" method="get">
+<select name="file">$files</select>
 <select name="group">$groups</select>
 <select name="date">$dates</select>
 <input type="submit" value="Show">
@@ -32,10 +41,12 @@ $timetable = DB::Query('
 	FROM Pairs p
 		JOIN Times t ON t.ID = p.TimeID
 		JOIN Styles s ON s.ID = p.StyleID
-	WHERE p.GroupID = :group
+	WHERE p.FileID = :file
+		AND p.GroupID = :group
 		AND p.DateID = :date
 	ORDER BY t.Number
 ', array(
+	':file' => $fileId,
 	':group' => $groupId,
 	':date' => $dateId,
 ));
