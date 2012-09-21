df37f1d89dad2a1d279df81efbdacfd104bb2423
Arseniy <spufru@gmail.com>
2012-09-21 21:55:51 +0600

    pretty

--- api.php
+++ api.php
@@ -20,7 +20,7 @@ switch ($api_ver) {
 
 if (version_compare(PHP_VERSION, '5.3.3') >= 0)
 	if (isset($_GET['pretty']))
-		$data = json_encode($data, JSON_NUMERIC_CHECK | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+		$data = prettyPrint(json_encode($data, JSON_NUMERIC_CHECK));
 	else
 		$data = json_encode($data, JSON_NUMERIC_CHECK);
 else
@@ -220,4 +220,55 @@ function api_3($data) {
 			$data['error'] = 'Неверный запрос';
 	}
 	return $data;
+}
+
+/**
+ * Pretty-print JSON string
+ *
+ * Use 'indent' option to select indentation string - by default it's a tab
+ *
+ * @param string $json Original JSON string
+ * @param array $options Encoding options
+ * @return string
+ */
+function prettyPrint($json, $options = array())
+{
+	$tokens = preg_split('|([\{\}\]\[,])|', $json, -1, PREG_SPLIT_DELIM_CAPTURE);
+	$result = "";
+	$indent = 0;
+
+	$ind = "\t";
+	if (isset($options['indent'])) {
+		$ind = $options['indent'];
+	}
+
+	$inLiteral = false;
+	foreach ($tokens as $token) {
+		if ($token == "") continue;
+
+		$prefix = str_repeat($ind, $indent);
+		if (!$inLiteral && ($token == "{" || $token == "[")) {
+			$indent++;
+			if ($result != "" && $result[strlen($result)-1] == "\n") {
+				$result .= $prefix;
+			}
+			$result .= "$token\n";
+		} elseif (!$inLiteral && ($token == "}" || $token == "]")) {
+			$indent--;
+			$prefix = str_repeat($ind, $indent);
+			$result .= "\n$prefix$token";
+		} elseif (!$inLiteral && $token == ",") {
+			$result .= "$token\n";
+		} else {
+			$result .= ($inLiteral ?  '' : $prefix) . $token;
+
+			// Count # of unescaped double-quotes in token, subtract # of
+			// escaped double-quotes and if the result is odd then we are
+			// inside a string literal
+			if ((substr_count($token, "\"")-substr_count($token, "\\\"")) % 2 != 0) {
+				$inLiteral = !$inLiteral;
+			}
+		}
+	}
+	return $result;
 }
