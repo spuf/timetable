83588ef6a39c5dba55acadeb56462fb66d1aa1a6
Arseniy <spufru@gmail.com>
2012-09-23 23:48:31 +0600

    screen

--- index.php
+++ index.php
@@ -206,7 +206,7 @@ HTML;
 	$content = <<<HTML
 <section id="gadget">
 <h4>Гаджет для Windows 7</h4>
-<p><img src="externals/gadget.png" class="img-polaroid" alt="gadget"></p>
+<p><img src="externals/gadget.png" class="img-polaroid" alt="Screenshot"></p>
 <p>
 Microsoft с июля 2012 года отключила установку сторонних гаджетов:
 <a href="http://windows.microsoft.com/ru-RU/windows/downloads/personalize/gadgets" target="_blank" rel='nofollow'>http://windows.microsoft.com/ru-RU/windows/downloads/personalize/gadgets</a>.<br>
@@ -219,6 +219,7 @@ Microsoft с июля 2012 года отключила установку сто
 
 <section id="android">
 <h4>Приложение для Android</h4>
+<p><img src="externals/android.png" class="img-polaroid" alt="Screenshot"></p>
 <p>
 <a href="https://play.google.com/store/apps/details?id=ru.spuf.timetable" target="_blank">Перейти на Google Play</a>
 </p>
@@ -226,7 +227,7 @@ Microsoft с июля 2012 года отключила установку сто
 
 <section id="chrome">
 <h4>Расширение для Chrome</h4>
-<p><img src="externals/extension.png" class="img-polaroid" alt="extension"></p>
+<p><img src="externals/extension.png" class="img-polaroid" alt="extScreenshotension"></p>
 <p>
 Чтобы установить расширение
 </p>
